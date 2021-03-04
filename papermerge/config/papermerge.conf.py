caad507073c6b5572fe09155c12f255e6dac3be7
Paolo Basso <basso.paolo99@gmail.com>
2021-03-04 18:15:19 +0100

    Add papermerge

---
+++ papermerge/config/papermerge.conf.py
@@ -0,0 +1,19 @@
+#DBTYPE = "mariadb"  # Uncomment this to enable an external DB instead of local SQLite, refer to Papermerge docs
+DBUSER = "root"
+DBPASS = "root"
+DBHOST = "mariadb"
+DBNAME = "papermerge"
+
+MEDIA_DIR = "/data/media"
+STATIC_DIR = "/app/papermerge/static"
+MEDIA_URL = "/media/"
+STATIC_URL = "/static/"
+
+OCR_DEFAULT_LANGUAGE = "eng"
+
+OCR_LANGUAGES = {
+    "eng": "English",
+    "ita": "Italiano",
+}
+
+IMPORTER_DIR = "/import"
